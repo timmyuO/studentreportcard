769fdd814b875e07587d32e73628fdf3c8fed1b2
timmyuO <111019271+timmyuO@users.noreply.github.com>
2022-08-20 22:57:53 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace studentreportcard
+{
+    partial class Reportcard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.projectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.windowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Scorebutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.assignbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.exambox = new System.Windows.Forms.TextBox();
+            this.testbox = new System.Windows.Forms.TextBox();
+            this.Scorebox = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.projectToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.windowToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1217, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(56, 29);
+            this.fileToolStripMenuItem.Text = "file ";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(58, 29);
+            this.editToolStripMenuItem.Text = "edit";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(63, 29);
+            this.viewToolStripMenuItem.Text = "view";
+            // 
+            // projectToolStripMenuItem
+            // 
+            this.projectToolStripMenuItem.Name = "projectToolStripMenuItem";
+            this.projectToolStripMenuItem.Size = new System.Drawing.Size(83, 29);
+            this.projectToolStripMenuItem.Text = "project";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(68, 29);
+            this.toolsToolStripMenuItem.Text = "tools";
+            // 
+            // windowToolStripMenuItem
+            // 
+            this.windowToolStripMenuItem.Name = "windowToolStripMenuItem";
+            this.windowToolStripMenuItem.Size = new System.Drawing.Size(90, 29);
+            this.windowToolStripMenuItem.Text = "window";
+            // 
+            // Scorebutton
+            // 
+            this.Scorebutton.Location = new System.Drawing.Point(837, 383);
+            this.Scorebutton.Name = "Scorebutton";
+            this.Scorebutton.Size = new System.Drawing.Size(117, 36);
+            this.Scorebutton.TabIndex = 1;
+            this.Scorebutton.Text = "Get Score";
+            this.Scorebutton.UseVisualStyleBackColor = true;
+            this.Scorebutton.Click += new System.EventHandler(this.Scorebutton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter assignment";
+            // 
+            // assignbox
+            // 
+            this.assignbox.Location = new System.Drawing.Point(301, 89);
+            this.assignbox.Name = "assignbox";
+            this.assignbox.Size = new System.Drawing.Size(100, 26);
+            this.assignbox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(113, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter test";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(113, 310);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Enter exam";
+            // 
+            // exambox
+            // 
+            this.exambox.Location = new System.Drawing.Point(301, 304);
+            this.exambox.Name = "exambox";
+            this.exambox.Size = new System.Drawing.Size(100, 26);
+            this.exambox.TabIndex = 6;
+            // 
+            // testbox
+            // 
+            this.testbox.Location = new System.Drawing.Point(301, 193);
+            this.testbox.Name = "testbox";
+            this.testbox.Size = new System.Drawing.Size(100, 26);
+            this.testbox.TabIndex = 7;
+            // 
+            // Scorebox
+            // 
+            this.Scorebox.Location = new System.Drawing.Point(1017, 388);
+            this.Scorebox.Name = "Scorebox";
+            this.Scorebox.Size = new System.Drawing.Size(100, 26);
+            this.Scorebox.TabIndex = 8;
+            // 
+            // Reportcard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1217, 450);
+            this.Controls.Add(this.Scorebox);
+            this.Controls.Add(this.testbox);
+            this.Controls.Add(this.exambox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.assignbox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Scorebutton);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Reportcard";
+            this.Text = "Reportcard";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem projectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem windowToolStripMenuItem;
+        private System.Windows.Forms.Button Scorebutton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox assignbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox exambox;
+        private System.Windows.Forms.TextBox testbox;
+        private System.Windows.Forms.TextBox Scorebox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace studentreportcard
+{
+    public partial class Reportcard : Form
+    {
+        public Reportcard()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Scorebutton_Click(object sender, EventArgs e)
+        {
+            int testScore = Int32.Parse (testbox.Text);
+            int assignmentScore = Int32.Parse(assignbox.Text);
+            int examScore = Int32.Parse(exambox.Text);
+            Scorebox.Text = TotalScore (testScore, assignmentScore, examScore).ToString();
+        }
+        public int TotalScore(int testScore, int AssignmentScore, int ExamScore)
+        {
+            return (testScore + AssignmentScore + ExamScore);
+        }
+    }  
+        
+}
